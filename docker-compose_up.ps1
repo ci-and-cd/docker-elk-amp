e352b13a85e2561f0ad1537429822d7c56bdec31
Haolun Zhang <haolun.zhang@n4l.co.nz>
2020-01-14 17:38:26 +1300

    fix config

--- docker-compose_up.ps1
+++ docker-compose_up.ps1
@@ -4,8 +4,9 @@ $env:PWD = ($p.substring(0,2).ToLower() + $p.substring(2)).replace("\", "/")
 Get-ChildItem Env:PWD
 
 $env:IMAGE_TAG = '7.5.1'
+$kibana_host = 'standalone.elk-kibana:5601'
 $elasticsearch = 'standalone.elk-elasticsearch:9200'
-$env:APM_COMMAND = "--strict.perms=false -e -E output.elasticsearch.hosts=[`"$elasticsearch`"]"
+$env:APM_COMMAND = "--strict.perms=false -e -E apm-server.kibana.host=`"$kibana_host`" -E output.elasticsearch.hosts=[`"$elasticsearch`"]"
 
 docker-compose down -v
 docker-compose up -d
